f51f34bf5cdc54a9ecf00497df2ba836ad1156cf
koohako <arturguimaraesdefreitas@gmail.com>
2020-09-08 17:14:05 -0300

    nova tela de add funcionario e view funcionario

--- src/main/java/br/com/codificando/controller/FuncionarioController.java
+++ src/main/java/br/com/codificando/controller/FuncionarioController.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.codificando.model.Funcionario;
 import br.com.codificando.repository.FuncionarioRepository;
@@ -31,5 +33,25 @@ public class FuncionarioController {
 		return "funcionario/add";
 	}
 	
+	@PostMapping("/funcionario/save")
+	public String saveFuncionario(Funcionario funcionario){
+	
+		try {
+			
+			if(funcionario != null)
+			funcionarioRepository.save(funcionario);
+		} catch (Exception e) {
+			System.out.print("Erro ao salvar: " + e.getMessage());
+		}
+		
+		return "redirect:/funcionario/view/" + funcionario.getId();
+	}
+	
+	@GetMapping("/funcionario/view/{id}")
+	public String viewFuncionario(@PathVariable long id, Model model) {
+			model.addAttribute("funcionario", funcionarioRepository.findById(id));
+			
+			return "funcionario/view";
+	}
 	
 }
